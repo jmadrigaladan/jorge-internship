ba44a325a5f546b7afa07adfeb1d14ec9b4b513a
jmadrigaladan <jorgemadrigaladan@gmail.com>
2022-11-21 22:03:43 -0800

    finished explore-items page

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,77 +1,84 @@
 import React from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import NewItem from "../home/NewItem.jsx";
+import LazyLoadNewItem from "../home/LazyLoadNewItem.jsx";
 
 const ExploreItems = () => {
+  const [exploreItems, setExploreItems] = React.useState();
+  const [index, setIndex] = React.useState(8);
+  const [itemsLength, setItemsLength] = React.useState();
+  const [filterValue, setFilterValue] = React.useState("");
+  React.useEffect(() => {
+    axios
+      .get(
+        `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore${
+          filterValue == "" ? "" : `?filter=${filterValue}`
+        }`
+      )
+      .then((response) => {
+        setExploreItems(response.data);
+        setItemsLength(Object.keys(response.data).length);
+      });
+  }, [filterValue]);
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select
+          id="filter-items"
+          defaultValue=""
+          onChange={(e) => {
+            setFilterValue(e.target.value);
+          }}
+        >
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
-          <div className="nft__item">
-            <div className="author_list_pp">
-              <Link
-                to="/author"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              >
-                <img className="lazy" src={AuthorImage} alt="" />
-                <i className="fa fa-check"></i>
-              </Link>
+      {exploreItems
+        ? exploreItems.slice(0, index).map((item) => (
+            <div
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              key={item.id}
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <NewItem
+                authorImage={item.authorImage}
+                expiryDate={item.expiryDate}
+                nftImage={item.nftImage}
+                title={item.title}
+                price={item.price}
+                likes={item.likes}
+                nftLink={item.nftId}
+              />
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
-
-            <div className="nft__item_wrap">
-              <div className="nft__item_extra">
-                <div className="nft__item_buttons">
-                  <button>Buy Now</button>
-                  <div className="nft__item_share">
-                    <h4>Share</h4>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-facebook fa-lg"></i>
-                    </a>
-                    <a href="" target="_blank" rel="noreferrer">
-                      <i className="fa fa-twitter fa-lg"></i>
-                    </a>
-                    <a href="">
-                      <i className="fa fa-envelope fa-lg"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
-              </Link>
+          ))
+        : new Array(8).fill(0).map((_, index) => (
+            <div
+              className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+              key={index}
+              style={{ display: "block", backgroundSize: "cover" }}
+            >
+              <LazyLoadNewItem />
             </div>
-            <div className="nft__item_info">
-              <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
-              </Link>
-              <div className="nft__item_price">1.74 ETH</div>
-              <div className="nft__item_like">
-                <i className="fa fa-heart"></i>
-                <span>69</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
+          ))}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
-          Load more
-        </Link>
+        {index !== itemsLength ? (
+          <Link
+            to=""
+            id="loadmore"
+            className="btn-main lead"
+            onClick={() => {
+              setIndex(index + 4);
+            }}
+          >
+            Load more
+          </Link>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
